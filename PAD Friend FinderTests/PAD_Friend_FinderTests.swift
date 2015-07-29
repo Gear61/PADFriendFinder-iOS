307c5337723813d94391d79aafd36983294a701c
Gear61 <chessnone@yahoo.com>
2015-07-28 20:13:01 -0700

    Initial files

---
+++ PAD Friend FinderTests/PAD_Friend_FinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  PAD_Friend_FinderTests.swift
+//  PAD Friend FinderTests
+//
+//  Created by Alexander Chiou on 7/28/15.
+//  Copyright (c) 2015 Alexander Chiou. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PAD_Friend_FinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
